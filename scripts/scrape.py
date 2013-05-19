b85eed2ed191567ee0c217fb2a40c331c9e0098a
crccheck <c@crccheck.com>
2013-05-19 02:41:42 +0000

    copy fab parse into scripts/scrape.py

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,43 +1,14 @@
+import datetime
 import os
+import time
 
-from fabric.api import *
-
-env.use_ssh_config = True
-env.hosts = ['dh']
+from dateutil import parser
+from lxml.html import parse
 
 DATA_DIR = "download"
-REMOTE_DATA_FILE = "conditions.tgz"
 
 
-@task
-def grab_files():
-    print env
-
-    project_root = os.path.dirname(env.real_fabfile)
-
-    with lcd(project_root):
-        local("mkdir -p %s" % DATA_DIR)
-
-        print project_root
-        with cd("ercot"):
-            local_file = os.path.join(DATA_DIR, REMOTE_DATA_FILE)
-            # archive remote files
-            run("tar -czf %s *.html && rm *.html" % REMOTE_DATA_FILE)
-            # download remote file
-            get(REMOTE_DATA_FILE, local_file)
-            # extract
-            local("cd %s && tar -xzf %s" % (DATA_DIR, REMOTE_DATA_FILE))
-            # delete compressed file
-            local("mv -f %s /tmp" % local_file)
-
-
-# DEPRECATED
-@task
-def parse(days=None):
-    import datetime
-    from dateutil import parser
-    import time
-    from lxml.html import parse
+def main(days=None):
     files = [os.path.join(DATA_DIR, f) for f in os.listdir(DATA_DIR) if f[-4:] == 'html']
     outfile = open("test.csv", "w")
     header_written = False
@@ -70,3 +41,7 @@ def parse(days=None):
         outfile.write(",".join([str(ctime), timestamp] + values))
         outfile.write("\n")
     outfile.close()
+
+
+if __name__ == "__main__":
+    main()
