cacd66c91c9cae5dc4dd3a53d794b83e56ab4dd0
crccheck <c@crccheck.com>
2013-05-19 02:52:51 +0000

    sketch storing data in sqlite

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,47 +1,37 @@
 from glob import glob
-import datetime
 import os
 import time
+import logging
 
 from dateutil import parser
 from lxml.html import parse
+import dataset
 
-DATA_DIR = "download"
+DATA_DIR = "../download"
 
 
-def main(days=None):
+def main():
+    logger = logging.getLogger(__name__)
+    db = dataset.connect('sqlite:///test.db')
+    table = db['ercot_realtime']
+
     files = glob(os.path.join(DATA_DIR, '*.html'))
-    outfile = open("test.csv", "w")
-    header_written = False
-    if days is not None:
-        start = datetime.datetime.now() - datetime.timedelta(days=int(days))
-    else:
-        start = datetime.datetime.utcfromtimestamp(0)
     for f in files:
         try:
             doc = parse(open(f, "r"))
             timestamp = doc.xpath("//span[@class='labelValueClass']")[0].text
             timestamp = timestamp.split(" ", 2)[2]
             created = parser.parse(timestamp)
-            if created < start:
-                continue
             ctime = int(time.mktime(created.timetuple()))
             labels = [x.text for x in doc.xpath("//span[@class='labelValueClass']")[1:]]
             values = [x.text for x in doc.xpath("//span[@class='labelValueClassBold']")]
-            data = zip(labels, values)
+            data = dict(zip(labels, values))
             # TODO delete file after parsing
-        except AssertionError:
+        except AssertionError as e:
+            logger.error(e)
             continue
-        #import ipdb; ipdb.set_trace()
-        #break
-        # print f, ctime
-        if header_written is False:
-            outfile.write("ctime,date," + ",".join(labels))
-            outfile.write("\n")
-            header_written = True
-        outfile.write(",".join([str(ctime), timestamp] + values))
-        outfile.write("\n")
-    outfile.close()
+        logger.info("{} {}".format(ctime, data))
+        table.insert(data)
 
 
 if __name__ == "__main__":
