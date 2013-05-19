c45b93b62eff8517df5922ee2f14d1d512b38562
crccheck <c@crccheck.com>
2013-05-19 03:59:31 +0000

    add test for normalize_html func

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 
 from scripts import scrape
@@ -16,7 +17,20 @@ class TestScraper(unittest.TestCase):
         self.assertEqual(type(output[should_be_float]), float)
 
 
-    @unittest.skip("TODO")
     def test_normalize_works(self):
-        # TODO, grab a file and run it through scrape.normalize_html
-        pass
+        control = {
+            'Actual System Demand': 31579,
+            'Current Frequency': 59.962,
+            'DC_E (East)': 0,
+            'DC_L (Laredo VFT)': 100,
+            'DC_N (North)': 25,
+            'DC_R (Railroad)': 151,
+            'DC_S (Eagle Pass)': 0,
+            'Instantaneous Time Error': -2.562,
+            'timestamp': datetime.datetime(2012, 3, 29, 23, 9, 50),
+            'Total System Capacity (not including Ancillary Services)': 38322,
+            'Total Wind Output (hourly average)': 5973,
+        }
+        with open('fixtures/test_download.html', 'r') as f:
+            data = scrape.normalize_html(f)
+            self.assertEqual(data, control)
