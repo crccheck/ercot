44cbc8045e517cd94b4b80af6f9c173f8f33974d
crccheck <c@crccheck.com>
2013-05-21 05:01:51 +0000

    add jsonp support

--- api_server/app.py
+++ api_server/app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from functools import wraps
+
+from flask import Flask, request
 app = Flask(__name__)
 
 import psycopg2
+
+
 conn = psycopg2.connect("dbname='ercot' ")
 
 
+# https://gist.github.com/aisipos/1094140
+def support_jsonp(f):
+    """Wraps JSONified output for JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f(*args, **kwargs).data) + ')'
+            return app.response_class(
+                content,
+                mimetype='application/javascript',
+            )
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
+
+
 @app.route('/')
+@support_jsonp
 def hello_world():
     cur = conn.cursor()
     n_results = 86400 / 10
@@ -16,7 +38,10 @@ def hello_world():
             FROM ercot_realtime ORDER BY timestamp LIMIT %s
         ) t;
         """, (n_results, ))
-    return cur.fetchone()[0]
+    return app.response_class(
+        cur.fetchone()[0],
+        mimetype="application/json",
+    )
 
 
 if __name__ == '__main__':
