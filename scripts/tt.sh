3d0cc64e98b084f3501791d7b4336905590da126
crccheck <c@crccheck.com>
2013-06-02 18:44:50 +0000

    add a plot of response times too
    at great cost because we have to run ab twice.

--- scripts/tt.sh
+++ scripts/tt.sh
@@ -1,12 +1,13 @@
 set -e
 
 PORT=8008
-AB_OPTIONS='-n 10 -w'
+AB_OPTIONS='-n 100'
 # options:
 #
 #   -n requests
 #   -c concurrency
 #   -w output html table
+#   -g gnuplot-file
 OUTFILE=../metrics/out.html
 
 
@@ -23,16 +24,36 @@ sleep 1
 
 
 bench() {
-  ab $AB_OPTIONS http://localhost:$PORT$1 >> $OUTFILE
+    ab $AB_OPTIONS -w http://localhost:$PORT$1 >> $OUTFILE
+    # can't do -w and -g at the same time
+    ab $AB_OPTIONS -g ../metrics/$2.tsv http://localhost:$PORT$1 > /dev/null
 }
 
 cp ../metrics/out_head_template.html $OUTFILE
-bench /pg/
-bench /py/
-bench /psy/
-bench /array/
-bench /array-sync/
+bench /psy/ psy
+bench /pg/ pg
+bench /py/ py
+bench /array/ array
+bench /array-sync/ array2
 echo '</section></body></html>' >> $OUTFILE
+
+cd ../metrics
+# http://tjholowaychuk.com/post/543349452/apachebench-gnuplot-graphing-benchmarks
+# plots manually ordered slowest to fastest
+
+gnuplot -e "set terminal png;
+set output \"out.png\";
+set key top left;
+set grid y;
+set ylabel \"response time (ms)\";
+plot
+  'psy.tsv' using 9 smooth sbezier with lines title 'json.dumps(RealDictCursor)',
+  'pg.tsv' using 9 smooth sbezier with lines title 'Postgres array_to_json',
+  'py.tsv' using 9 smooth sbezier with lines title 'json.dumps(dict)',
+  'array.tsv' using 9 smooth sbezier with lines title 'json.dumps(list) async',
+  'array2.tsv' using 9 smooth sbezier with lines title 'json.dumps(list) sync';
+"
+
 
 # kill server, run in a subprocess so we can suppress "Terminated" message
 (kill $PID 2>&1) > /dev/null
