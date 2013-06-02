4563214622e96f2e4b91d8fc7ad8d2a7a144d392
crccheck <c@crccheck.com>
2013-06-02 17:27:22 +0000

    make apachebench output to html

--- scripts/tt.sh
+++ scripts/tt.sh
@@ -1,6 +1,14 @@
 set -e
 
 PORT=8008
+AB_OPTIONS='-n 10 -w'
+# options:
+#
+#   -n requests
+#   -c concurrency
+#   -w output html table
+OUTFILE=../metrics/out.html
+
 
 # start server in the background
 python ../tt_api/api.py --port=$PORT --logging=none &
@@ -13,17 +21,13 @@ trap "kill $PID; echo bye $PID" EXIT
 # give time for the server to get up
 sleep 1
 
-mkdir -p ../metrics
-ab -n 100 http://localhost:$PORT/pg/ > ../metrics/tornado_pg.log
-ab -n 100 http://localhost:$PORT/py/ > ../metrics/tornado_py.log
-ab -n 100 http://localhost:$PORT/psy/ > ../metrics/tornado_psy.log
-ab -n 100 http://localhost:$PORT/array/ > ../metrics/tornado_array.log
-ab -n 100 http://localhost:$PORT/array-sync/ > ../metrics/tornado_arraysync.log
-# ab -n 100 -c 2 http://localhost:$PORT/ > ../metrics/tornadox2.log
-# options:
-#
-#   -n requests
-#   -c concurrency
+cp ../metrics/out_head_template.html $OUTFILE
+ab $AB_OPTIONS http://localhost:$PORT/pg/ >> $OUTFILE
+ab $AB_OPTIONS http://localhost:$PORT/py/ >> $OUTFILE
+ab $AB_OPTIONS http://localhost:$PORT/psy/ >> $OUTFILE
+ab $AB_OPTIONS http://localhost:$PORT/array/ >> $OUTFILE
+ab $AB_OPTIONS http://localhost:$PORT/array-sync/ >> $OUTFILE
+echo '</body></html>' >> $OUTFILE
 
 # kill server, run in a subprocess so we can suppress "Terminated" message
 (kill $PID 2>&1) > /dev/null
