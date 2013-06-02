f1fbb081d54630a7d5f08c9249611eb24ee6fc74
crccheck <c@crccheck.com>
2013-06-02 21:22:13 +0000

    refactor: generate plots in bench function

--- scripts/tt.sh
+++ scripts/tt.sh
@@ -22,37 +22,33 @@ trap "kill $PID; echo bye $PID" EXIT
 # give time for the server to get up
 sleep 1
 
+plots=''
 
+# params: url alias title
 bench() {
     ab $AB_OPTIONS -w http://localhost:$PORT$1 >> $OUTFILE
     # can't do -w and -g at the same time
     ab $AB_OPTIONS -g ../metrics/$2.tsv http://localhost:$PORT$1 > /dev/null
+    plots="$plots '$2.tsv' using 9 smooth sbezier with lines title '$3',"
 }
 
 cp ../metrics/out_head_template.html $OUTFILE
-bench /psy/ psy
-bench /pg/ pg
-bench /py/ py
-bench /array/ array
-bench /array-sync/ array2
+bench /psy/ psy "json.dumps(RealDictCursor)"
+bench /pg/ pg Postgres "array_to_json"
+bench /py/ py "json.dumps(dict)"
+bench /array/ array "json.dumps(list) async"
+bench /array-sync/ array2 "json.dumps(list) sync"
 echo '</section></body></html>' >> $OUTFILE
 
 cd ../metrics
 # http://tjholowaychuk.com/post/543349452/apachebench-gnuplot-graphing-benchmarks
-# plots manually ordered slowest to fastest
 
 gnuplot -e "set terminal png;
 set output \"out.png\";
 set key top left;
 set grid y;
 set ylabel \"response time (ms)\";
-plot
-  'psy.tsv' using 9 smooth sbezier with lines title 'json.dumps(RealDictCursor)',
-  'pg.tsv' using 9 smooth sbezier with lines title 'Postgres array_to_json',
-  'py.tsv' using 9 smooth sbezier with lines title 'json.dumps(dict)',
-  'array.tsv' using 9 smooth sbezier with lines title 'json.dumps(list) async',
-  'array2.tsv' using 9 smooth sbezier with lines title 'json.dumps(list) sync';
-"
+plot ${plots%?};"
 
 
 # kill server, run in a subprocess so we can suppress "Terminated" message
